75f0ceb658152a6881ba3183da37d70af4059fb1
Dennis Dadey <deedadey@gmail.com>
2023-04-12 21:07:39 -0700

    read a utf8 file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""A funciton to read file"""
 
 def read_file(filename=""):
+    """open and read file"""
     with open(filename, "r", encoding="utf-8") as new_read:
         print(new_read.read())
